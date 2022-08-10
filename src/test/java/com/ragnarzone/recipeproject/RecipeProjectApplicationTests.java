63f24fd84ffd3dd95fe6b9d8389cd96e4ea8873e
ragnarzone <ragnarzone@gmail.com>
2022-08-10 09:45:00 +0300

    Initial commit

---
+++ src/test/java/com/ragnarzone/recipeproject/RecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ragnarzone.recipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
