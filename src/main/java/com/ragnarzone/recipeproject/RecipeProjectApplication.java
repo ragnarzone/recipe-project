c8aad5a350b48d440fe729122c6af6b37e682f4a
ragnarzone <ragnarzone@gmail.com>
2022-08-13 08:12:26 +0300

    added controller and index

--- src/main/java/com/ragnarzone/recipeproject/RecipeProjectApplication.java
+++ src/main/java/com/ragnarzone/recipeproject/RecipeProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecipeProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RecipeProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeProjectApplication.class, args);
+    }
 
 }
