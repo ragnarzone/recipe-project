51a8f4d6b505ae198fa07d83f71792eb8b95ff62
ragnarzone <ragnarzone@gmail.com>
2022-08-13 09:38:07 +0300

    added UnitOfMeasure entity and bounded to Ingredient as One to One

--- src/main/java/com/ragnarzone/recipeproject/domain/Ingredient.java
+++ src/main/java/com/ragnarzone/recipeproject/domain/Ingredient.java
@@ -13,10 +13,11 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    // private UnitOfMeasure uom;
-
     @ManyToOne
     private Recipe recipe;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure unitOfMeasure;
 
     public Long getId() {
         return id;
@@ -48,6 +49,14 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
     }
 }
 
