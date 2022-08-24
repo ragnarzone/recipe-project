b0560fe85c03cda3ba56f4d5369a149e38b87fe9
ragnarzone <ragnarzone@gmail.com>
2022-08-24 22:32:06 +0300

    added ArgumentCaptor logic for IndexController

--- src/test/java/com/ragnarzone/recipeproject/controllers/IndexControllerTest.java
+++ src/test/java/com/ragnarzone/recipeproject/controllers/IndexControllerTest.java
@@ -1,17 +1,20 @@
 package com.ragnarzone.recipeproject.controllers;
 
+import com.ragnarzone.recipeproject.domain.Recipe;
 import com.ragnarzone.recipeproject.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class IndexControllerTest {
 
@@ -32,8 +35,25 @@ public class IndexControllerTest {
 
     @Test
     public void getIndexPage() {
+
+        // given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+
+        Recipe recipeWithId = new Recipe();
+        recipeWithId.setId(1L);
+        recipes.add(recipeWithId);
+
+        // when
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        // then
         assertEquals("index", indexController.getIndexPage(model));
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        assertEquals(2, argumentCaptor.getValue().size());
     }
 }
