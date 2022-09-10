f7aa5a9f9c813eb84f7f5b97132a3a70f8842e37
ragnarzone <ragnarzone@gmail.com>
2022-09-10 12:02:36 +0300

    completed RecipeCommandToRecipe

--- src/main/java/com/ragnarzone/recipeproject/converters/RecipeCommandToRecipe.java
+++ src/main/java/com/ragnarzone/recipeproject/converters/RecipeCommandToRecipe.java
@@ -8,6 +8,16 @@ import org.springframework.lang.Nullable;
 
 public class RecipeCommandToRecipe implements Converter<RecipeCommand, Recipe> {
 
+    private final CategoryCommandToCategory categoryConverter;
+    private final IngredientCommandToIngredient ingredientConverter;
+    private final NotesCommandToNotes notesConverter;
+
+    public RecipeCommandToRecipe(CategoryCommandToCategory categoryConverter, IngredientCommandToIngredient ingredientConverter, NotesCommandToNotes notesConverter) {
+        this.categoryConverter = categoryConverter;
+        this.ingredientConverter = ingredientConverter;
+        this.notesConverter = notesConverter;
+    }
+
     @Synchronized
     @Nullable
     @Override
@@ -26,5 +36,18 @@ public class RecipeCommandToRecipe implements Converter<RecipeCommand, Recipe> {
         recipe.setServings(source.getServings());
         recipe.setSource(source.getSource());
         recipe.setUrl(source.getUrl());
+        recipe.setNotes(notesConverter.convert(source.getNotes()));
+
+        if (source.getCategories() != null && source.getCategories().size() > 0){
+            source.getCategories()
+                    .forEach( category -> recipe.getCategories().add(categoryConverter.convert(category)));
+        }
+
+        if (source.getIngredients() != null && source.getIngredients().size() > 0){
+            source.getIngredients()
+                    .forEach(ingredient -> recipe.getIngredients().add(ingredientConverter.convert(ingredient)));
+        }
+
+        return recipe;
     }
 }
