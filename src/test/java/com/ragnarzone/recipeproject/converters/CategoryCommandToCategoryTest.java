59cca1cbefcb1f4dc51f2348268729fe141ce96c
ragnarzone <ragnarzone@gmail.com>
2022-09-10 12:29:43 +0300

    added test for CategoryCommandToCategory

---
+++ src/test/java/com/ragnarzone/recipeproject/converters/CategoryCommandToCategoryTest.java
@@ -0,0 +1,45 @@
+package com.ragnarzone.recipeproject.converters;
+
+import com.ragnarzone.recipeproject.commands.CategoryCommand;
+import com.ragnarzone.recipeproject.domain.Category;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryCommandToCategoryTest {
+
+    public static final Long ID_VALUE = new Long(1l);
+    public static final String DESCRIPTION = "description";
+    CategoryCommandToCategory converter;
+
+    @Before
+    public void setUp() throws Exception {
+        converter = new CategoryCommandToCategory();
+    }
+
+    @Test
+    public void testNullObject() {
+        assertNull(converter.convert(null));
+    }
+
+    @Test
+    public void testEmptyObject() {
+        assertNotNull(converter.convert(new CategoryCommand()));
+    }
+
+    @Test
+    public void convert() {
+        //given
+        CategoryCommand categoryCommand = new CategoryCommand();
+        categoryCommand.setId(ID_VALUE);
+        categoryCommand.setDescription(DESCRIPTION);
+
+        //when
+        Category category = converter.convert(categoryCommand);
+
+        //then
+        assertEquals(ID_VALUE, category.getId());
+        assertEquals(DESCRIPTION, category.getDescription());
+    }
+}
