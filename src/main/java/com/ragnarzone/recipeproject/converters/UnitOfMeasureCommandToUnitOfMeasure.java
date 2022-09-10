16b2ec4452596e3ab391498adb86953e2ac5ef3d
ragnarzone <ragnarzone@gmail.com>
2022-09-10 11:16:26 +0300

    added UnitOfMeasureToUnitOfMeasureCommand

--- src/main/java/com/ragnarzone/recipeproject/converters/UnitOfMeasureCommandToUnitOfMeasure.java
+++ src/main/java/com/ragnarzone/recipeproject/converters/UnitOfMeasureCommandToUnitOfMeasure.java
@@ -5,7 +5,9 @@ import com.ragnarzone.recipeproject.domain.UnitOfMeasure;
 import lombok.Synchronized;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UnitOfMeasureCommandToUnitOfMeasure implements Converter<UnitOfMeasureCommand, UnitOfMeasure> {
 
     @Synchronized
---
+++ src/main/java/com/ragnarzone/recipeproject/converters/UnitOfMeasureToUnitOfMeasureCommand.java
@@ -0,0 +1,26 @@
+package com.ragnarzone.recipeproject.converters;
+
+import com.ragnarzone.recipeproject.commands.UnitOfMeasureCommand;
+import com.ragnarzone.recipeproject.domain.UnitOfMeasure;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UnitOfMeasureToUnitOfMeasureCommand implements Converter<UnitOfMeasure, UnitOfMeasureCommand> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public UnitOfMeasureCommand convert(UnitOfMeasure unitOfMeasure) {
+
+        if (unitOfMeasure != null) {
+            final UnitOfMeasureCommand uomc = new UnitOfMeasureCommand();
+            uomc.setId(unitOfMeasure.getId());
+            uomc.setDescription(unitOfMeasure.getDescription());
+            return uomc;
+        }
+        return null;
+    }
+}
