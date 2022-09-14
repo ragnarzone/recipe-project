cbb0c5431aa5ee7fe7d46320303fcb79b5b76a25
ragnarzone <ragnarzone@gmail.com>
2022-09-14 10:53:46 +0300

    added unit test for getRecipeById

---
+++ src/test/java/com/ragnarzone/recipeproject/services/RecipeServiceImplTest.java
@@ -0,0 +1,52 @@
+package com.ragnarzone.recipeproject.services;
+
+import com.ragnarzone.recipeproject.converters.RecipeCommandToRecipe;
+import com.ragnarzone.recipeproject.converters.RecipeToRecipeCommand;
+import com.ragnarzone.recipeproject.domain.Recipe;
+import com.ragnarzone.recipeproject.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Mock
+    RecipeToRecipeCommand recipeToRecipeCommand;
+
+    @Mock
+    RecipeCommandToRecipe recipeCommandToRecipe;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository,recipeCommandToRecipe, recipeToRecipeCommand);
+    }
+
+    @Test
+    public void getRecipeByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1l);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1l);
+
+        assertNotNull("Null recipe returned", recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+}
