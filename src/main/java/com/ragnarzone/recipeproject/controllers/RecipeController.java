be30aa5bce6492db3bbf3682496073252a5a6d1d
ragnarzone <ragnarzone@gmail.com>
2022-09-15 11:11:40 +0300

    added Controller method to show new recipe form and added saving method to save data after post request

--- src/main/java/com/ragnarzone/recipeproject/controllers/RecipeController.java
+++ src/main/java/com/ragnarzone/recipeproject/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package com.ragnarzone.recipeproject.controllers;
 
+import com.ragnarzone.recipeproject.commands.RecipeCommand;
 import com.ragnarzone.recipeproject.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,6 +24,20 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(id));
 
         return "recipe/show";
+    }
 
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
