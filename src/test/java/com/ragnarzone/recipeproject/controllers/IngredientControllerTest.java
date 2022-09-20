830050e179c9a8ede85e7588a9f86ab2857b8b17
ragnarzone <ragnarzone@gmail.com>
2022-09-20 12:04:45 +0300

    added unit test for ingredient show method

--- src/test/java/com/ragnarzone/recipeproject/controllers/IngredientControllerTest.java
+++ src/test/java/com/ragnarzone/recipeproject/controllers/IngredientControllerTest.java
@@ -1,6 +1,8 @@
 package com.ragnarzone.recipeproject.controllers;
 
+import com.ragnarzone.recipeproject.commands.IngredientCommand;
 import com.ragnarzone.recipeproject.commands.RecipeCommand;
+import com.ragnarzone.recipeproject.services.IngredientService;
 import com.ragnarzone.recipeproject.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +19,8 @@ public class IngredientControllerTest {
 
     @Mock
     RecipeService recipeService;
+    @Mock
+    IngredientService ingredientService;
     IngredientController controller;
     MockMvc mockMvc;
 
@@ -25,7 +29,7 @@ public class IngredientControllerTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
 
-        controller = new IngredientController(recipeService);
+        controller = new IngredientController(recipeService, ingredientService);
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
@@ -43,5 +47,21 @@ public class IngredientControllerTest {
 
         //then
         verify(recipeService, times(1)).findCommandById(anyLong());
+    }
+
+    @Test
+    public void testShowIngredient() throws Exception {
+        //given
+        IngredientCommand ingredientCommand = new IngredientCommand();
+
+        //when
+        when(ingredientService.findByRecipeIdAndIngredientId(anyLong(), anyLong()))
+                .thenReturn(ingredientCommand);
+
+        //then
+        mockMvc.perform(get("/recipe/1/ingredient/2/show"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/show"))
+                .andExpect(model().attributeExists("ingredient"));
     }
 }
